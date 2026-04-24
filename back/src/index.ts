d6274da26f4a3a0d274640998a4234a317188cd7
AngelaMiniello <angela.min03@gmail.com>
2026-04-24 16:33:50 -0300

    fix: use process.env.PORT for render

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,21 @@
 import server from "./server";
-import { PORT } from "./config/envs";
 import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 AppDataSource.initialize()
-    .then(res  => {
-        console.log("Conexión a la base de datos realizada con éxito")
-        server.listen(PORT, ()  => {
-            console.log(`Server listening on PORT ${PORT}`);
-        })
-});
+  .then(() => {
+    console.log("Conexión a la base de datos realizada con éxito");
+
+    server.listen(PORT, () => {
+      console.log(`Server listening on PORT ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al conectar DB:", error);
+  });
 
