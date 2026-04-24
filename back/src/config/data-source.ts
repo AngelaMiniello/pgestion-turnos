dbfd996b3969cdb428f623d496aaa049bbbbd0d5
AngelaMiniello <angela.min03@gmail.com>
2026-04-24 16:48:08 -0300

    fix: datasource for render db

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,19 +1,35 @@
-import "reflect-metadata"; //hace que los decoradores funcionen
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
 import { Credential } from "../entities/Credentials";
 import { Appointment } from "../entities/Appointments";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const isProduction = !!process.env.DATABASE_URL;
 
 export const AppDataSource = new DataSource({
-type: "postgres",
-host: "localhost",
-port: 5432,
-username: "postgres",
-password: "CristoRey",
-database: "gestor_turnos",
-synchronize: true,
-logging: false,
-entities: [User, Credential, Appointment],
-subscribers: [],
-migrations: [],
+  type: "postgres",
+
+  // 👉 producción (Render)
+  url: process.env.DATABASE_URL,
+
+  // 👉 local (tu compu)
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "CristoRey",
+  database: process.env.DB_NAME || "gestor_turnos",
+
+  synchronize: true,
+  logging: false,
+  entities: [User, Credential, Appointment],
+  subscribers: [],
+  migrations: [],
+
+  // 🔥 clave para Render
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
 });
