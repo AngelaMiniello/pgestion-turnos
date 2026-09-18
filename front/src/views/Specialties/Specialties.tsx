e030fee9ef506e0e34b317763faab0c446f1da9d
AngelaMiniello <angela.min03@gmail.com>
2026-09-17 22:32:56 -0300

    border

--- front/src/views/Specialties/Specialties.tsx
+++ front/src/views/Specialties/Specialties.tsx
@@ -305,7 +305,7 @@ function Specialties() {
           return ( 
             <article 
               key={item.id} 
-              className={` overflow-hidden rounded-2xl border bg-white transition-all duration-300`} 
+              className={` overflow-hidden rounded-2xl bg-white transition-all duration-300`} 
             > 
               
               {/* Botón */} 
@@ -321,7 +321,8 @@ function Specialties() {
                 text-left outline-none 
                 transition-colors duration-200 
                 hover:bg-[#f8fbff] focus:outline-none 
-                focus-visible:outline-none sm:px-7"
+                focus-visible:outline-none sm:px-7
+                focus:ring-0 focus:ring-offset-0"
               > 
                 
                 <div className="flex min-w-0 items-center gap-4">   
