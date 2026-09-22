c8aa567e5a56f1b817262281de043854e2195188
AngelaMiniello <angela.min03@gmail.com>
2026-09-22 19:54:49 -0300

    index

--- back/src/routes/indexRouter.ts
+++ back/src/routes/indexRouter.ts
@@ -1,19 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./usersRouter";
 import appointmentsRouter from "./appointmentsRouter";
-import { createUserController, getAllUsersContoller, getUserByIdConroller, registerUserController, loginUserController} from "../controllers/User.Controller";
-import auth from "../middlewares/autenticación";
 
-const indexRouter:Router = Router();
+const indexRouter: Router = Router();
 
-// Monta cada módulo en su ruta base
+// Monta cada módulo en su respectiva ruta base de forma limpia
 indexRouter.use("/users", usersRouter);
 indexRouter.use("/appointments", appointmentsRouter);
-
-indexRouter.use("/users", createUserController);
-indexRouter.use("/users", getAllUsersContoller);
-indexRouter.use("/users", auth, getUserByIdConroller);
-indexRouter.use("/users", registerUserController);
-indexRouter.use("/users", loginUserController);
 
 export default indexRouter;
